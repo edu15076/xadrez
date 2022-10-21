0862613dac230414b2df143911004722b4be8aac
Arthur-Teles <104380993+Arthur-Teles@users.noreply.github.com>
2022-10-21 15:29:48 -0300

    Movimentos possíveis das peças
    Desconsidera posições de peças amigas e inimigas ainda

--- scripts/chess.js
+++ scripts/chess.js
@@ -1,20 +1,146 @@
-verifyBordBarriers = (x, y) => (x <= 7 && x >= 0 && y <= 7 && y >= 0);
+// ---------ínicio tabuleiro----------
+let square = {
+    aWhite: false,
+    aBlack: false,
+    piece: 'none'
+};
 
-const king = {
+let matrizQuadrados = [[],[],[],[],[],[],[],[]];
+
+for (let i = 0; i < 8; i++)
+    for (let j = 0; j < 8; j++)
+        matrizQuadrados[i][j] = square;
+
+let verifyBordBarriers = (x, y) => (x <= 7 && x >= 0 && y <= 7 && y >= 0);
+
+// ---------Fim tabuleiro----------
+
+// ---------Início peças----------
+
+let king = {
+    type: 'white',
     x: 4,
     y: 7,
     moves: (x, y) => {
-        let possibleMoves = [];
+        let possibleKingMoves = [];
         for (let i = 0; i < 8; i++) {
             let inX = x + Math.cos(Math.PI / 4 * i), inY = y + Math.sin(Math.PI / 4 * i);
             inX = inX.toFixed(0);
             inY = inY.toFixed(0);
             if (verifyBordBarriers(inX, inY))
-                possibleMoves.push([inX, inY]);
+                possibleKingMoves.push([inX, inY]);
         }
-        return possibleMoves;
+        return possibleKingMoves;
     }
 };
+
+let knight = {
+    type: 'white',
+    x: 1,
+    y: 7,
+    moves: (x, y) => {
+        const arrKnight = [[x-1, y-2], [x+1, y-2], [x+2, y-1], [x+2, y+1], [x+1, y+2], [x-1, y+2], [x-2, y+1], [x-2, y-1]];
+        let possibleKnightMoves = [];
+        for (let i = 0; i < 8; i++) {
+            if (verifyBordBarriers(arrKnight[i]))
+                possibleKnightMoves.push(arrKnight[i]);
+        }
+        return possibleKnightMoves;
+    }
+}
+
+/*let pawn = {
+    type: 'white',
+    x: 0,
+    y: 6,
+    moves: (x,y) => {
+        //quando for possível verificar se tem openentes nas diagonais, considerar os possíveis lances de (x-1, y-1) e (x+1, y-1)
+    }
+}*/
+
+let bishop = {
+    type: 'white',
+    x: 2,
+    y: 7,
+    moves: (x, y) => {
+        let i; let j;
+        let possibleBishopMoves = [];
+        for (i = x, j = y; i >= 0 && j >= 0; i--, j--) {
+            possibleBishopMoves.push([i, j]);
+        }
+        for (i = x, j = y; i >= 7 && j >= 0; i++, j--) {
+            possibleBishopMoves.push([i, j]);
+        }
+        for (i = x, j = y; i >= 0 && j <= 7; i--, j++) {
+            possibleBishopMoves.push([i, j]);
+        }
+        for (i = x, j = y; i >= 7 && j <= 7; i++, j++) {
+            possibleBishopMoves.push([i, j]);
+        }
+        return possibleBishopMoves;
+    }
+}
+
+let rock = {
+    type: 'white',
+    x: 0,
+    y: 7,
+    moves: (x, y) => {
+        let possibleRockMoves = [];
+        let i;
+        for (i = x; i >= 0 ; i--) {
+            possibleRockMoves.push([i, y]);
+        }
+        for (i = x; i < 8; i++) {
+            possibleRockMoves.push([i, y]);
+        }
+        for (i = y; i >= 0; i--) {
+            possibleRockMoves.push([x, i]);
+        }
+        for (i = y; i < 8; i++) {
+            possibleRockMoves.push([x, i]);
+        }
+        return possibleRockMoves;
+    }
+}
+
+let queen = {
+    type: 'white',
+    x: 3,
+    y: 7,
+    moves: (x, y) => {
+        let i; let j;
+        let possibleQueenMoves = [];
+        for (i = x, j = y; i >= 0 && j >= 0; i--, j--) {
+            possibleQueenMoves.push([i, j]);
+        }
+        for (i = x, j = y; i >= 7 && j >= 0; i++, j--) {
+            possibleQueenMoves.push([i, j]);
+        }
+        for (i = x, j = y; i >= 0 && j <= 7; i--, j++) {
+            possibleQueenMoves.push([i, j]);
+        }
+        for (i = x, j = y; i >= 7 && j <= 7; i++, j++) {
+            possibleQueenMoves.push([i, j]);
+        }
+        for (i = x; i >= 0 ; i--) {
+            possibleQueenMoves.push([i, y]);
+        }
+        for (i = x; i < 8; i++) {
+            possibleQueenMoves.push([i, y]);
+        }
+        for (i = y; i >= 0; i--) {
+            possibleQueenMoves.push([x, i]);
+        }
+        for (i = ys; i < 8; i++) {
+            possibleQueenMoves.push([x, i]);
+        }
+        return possibleQueenMoves;
+    }
+}
+
+
+// ---------Fim peças----------
 
 /*const whiteKing = Object.create(king);
 mat = whiteKing.moves(3, 7);
@@ -23,3 +149,4 @@ for (let i = 0; i < mat.length; i++)
 
 const blackKing = Object.create(king);
 blackKing.y = 0;*/
+
