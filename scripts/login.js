8cbbbdf700d5d5bd2a7bcf02e4fb64fb4135ec6f
el0y-c0sm0 <eloymaciel06@gmail.com>
2022-10-23 14:28:49 -0300

    coloquei a pagina sobre a gente

--- scripts/login.js
+++ scripts/login.js
@@ -8,6 +8,66 @@ let loginEl = document.querySelector('#login');
 let mainEl = document.querySelector('main');
 let botaoLoginEl = document.querySelector('#login-btn');
 
+let novoNomeInput = document.querySelector('#novo-nome');
+let idadeInput = document.querySelector('#idade');
+let novaSenhaInput = document.querySelector('#nova-senha');
+
+let sumir = () => {
+    loginEl.style.transition = 'none';
+        loginEl.style.left = '-100vh';
+        userEl.style.transition = 'none';
+        userEl.style.left = '-100vh';
+        idadeEl.style.transition = 'none';
+        idadeEl.style.left = '-100vh';
+        senhaEl.style.transition = 'none';
+        senhaEl.style.left = '-100vh';
+        botaoConfirmar.style.transition = 'none';
+        botaoConfirmar.style.left = '-100vh';
+}
+
+let criaPerfil = () => {
+    let loginIncompleto = 0;
+
+    if(!novoNomeInput.value) {
+        novoNomeInput.placeholder="Digite um nome válido";
+        novoNomeInput.style.border="0.4vh solid rgb(136, 0, 0)";
+
+        loginIncompleto++;
+    }
+    if(!novaSenhaInput.value) {
+        novaSenhaInput.placeholder="Digite uma senha válida";
+        novaSenhaInput.style.border="0.4vh solid rgb(136, 0, 0)";
+        
+        loginIncompleto++;
+    }
+    if(!idadeInput.value) {
+        idadeInput.placeholder="Selecione uma idade válida";
+        idadeInput.style.border="0.4vh solid rgb(136, 0, 0)";
+        
+        loginIncompleto++;
+    }
+
+    if(loginIncompleto !== 0) {
+        return;
+    }
+
+    localStorage.removeItem('usuario');
+
+    let usuario = {
+        nome: novoNomeInput.value,
+        idade: idadeEl.value,
+        senha: novaSenhaInput.value,
+        score: 0,
+        vitorias: 0,
+        derrotas: 0,
+        empates: 0
+    }
+
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+
+    sumir();
+}
+
 botaoConta.addEventListener('click', () => {
     botaoConta.style.left = '-100vh';
     userEl.style.left = '84.2vh';
@@ -19,7 +79,7 @@ botaoConta.addEventListener('click', () => {
     botaoConfirmar.style.left = '92vh';
     botaoConfirmar.style.top = '89vh';
 });
-
+/*
 for (let botao of botoesSumir) {
     botao.addEventListener('click', () => {
         loginEl.style.transition = 'none';
@@ -34,15 +94,7 @@ for (let botao of botoesSumir) {
         botaoConfirmar.style.left = '-100vh';
     });
 }
-mainEl.addEventListener('click', () => {
-    loginEl.style.transition = 'none';
-    loginEl.style.left = '-100vh';
-    userEl.style.transition = 'none';
-    userEl.style.left = '-100vh';
-    idadeEl.style.transition = 'none';
-    idadeEl.style.left = '-100vh';
-    senhaEl.style.transition = 'none';
-    senhaEl.style.left = '-100vh';
-    botaoConfirmar.style.transition = 'none';
-    botaoConfirmar.style.left = '-100vh';
-});
+*/
+mainEl.addEventListener('click', sumir);
+
+botaoConfirmar.addEventListener('click', criaPerfil);
