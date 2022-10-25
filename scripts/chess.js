0d00d7db491f18c5a7448f528c16a251dcab5b84
edu15076 <eduardbh2014@gmail.com>
2022-10-24 22:50:46 -0300

    Os peões agora podem mover duas casas para frente na primeira mexida e agora ele come para os lados. O rei não pode mais se por em xeque ou outras peças, da mesma cor, daixarem ele em xeque. Inseri, também, a lógica dos turnos, entre as brancas es as pretas. Por fim, mudei as cores dos tabuleiros.

--- scripts/chess.js
+++ scripts/chess.js
@@ -1,9 +1,9 @@
 // ---------ínicio tabuleiro----------
-
+let turn = 'white';
 
 let square = {
-    aWhite: false,
-    aBlack: false,
+    whiteAttack: false,
+    blackAttack: false,
     piece: null
 };
 
@@ -14,78 +14,80 @@ function originalBoardSettings() {
         for (let j = 0; j < 8; j++) {
             board[i][j] = Object.create(square);
             if (j === 3 || j === 4)
-                board[i][j].aWhite = board[i][j].aBlack = false; 
+                board[i][j].whiteAttack = board[i][j].blackAttack = false; 
             else if (j === 5)
-                board[i][j].aWhite = true;
+                board[i][j].whiteAttack = true;
             else if (j === 2)
-                board[i][j].aBlack = true;
+                board[i][j].blackAttack = true;
             else if (j === 0) {
-                board[i][j].aWhite = false;
+                board[i][j].whiteAttack = false;
                 if (i === 0 || i === 7) {
-                    board[i][j].aBlack = false;
+                    board[i][j].blackAttack = false;
                     board[i][j].piece = Object.create(rook);
                     board[i][j].piece.x = i;
                     board[i][j].piece.y = 0;
                     board[i][j].piece.color = 'black';
                 } else if (i === 1 || i === 6) {
-                    board[i][j].aBlack = true;
+                    board[i][j].blackAttack = true;
                     board[i][j].piece = Object.create(knight);
                     board[i][j].piece.x = i;
                     board[i][j].piece.y = 0;
                     board[i][j].piece.color = 'black';
                 } else if (i === 2 || i === 5) {
-                    board[i][j].aBlack = true;
+                    board[i][j].blackAttack = true;
                     board[i][j].piece = Object.create(bishop);
                     board[i][j].piece.x = i;
                     board[i][j].piece.y = 0;
                     board[i][j].piece.color = 'black';
                 } else if (i === 3) {
-                    board[i][j].aBlack = true;
+                    board[i][j].blackAttack = true;
                     board[i][j].piece = Object.create(queen);
                     board[i][j].piece.y = 0;
                     board[i][j].piece.color = 'black';
                 } else {
-                    board[i][j].aBlack = true;
+                    board[i][j].blackAttack = true;
                     board[i][j].piece = Object.create(king);
                     board[i][j].piece.y = 0;
                     board[i][j].piece.color = 'black';
                 }
             } else if (j === 1) {
-                board[i][j].aWhite = false;
-                board[i][j].aBlack = true;
+                board[i][j].whiteAttack = false;
+                board[i][j].blackAttack = true;
                 board[i][j].piece = Object.create(pawn);
                 board[i][j].piece.x = i;
                 board[i][j].piece.y = 1;
                 board[i][j].piece.color = 'black';
             } else if (j === 6) {
-                board[i][j].aBlack = false;
-                board[i][j].aWhite = true;
+                board[i][j].blackAttack = false;
+                board[i][j].whiteAttack = true;
                 board[i][j].piece = Object.create(pawn);
                 board[i][j].piece.x = i;
             } else if (j === 7) {
-                board[i][j].aBlack = false;
+                board[i][j].blackAttack = false;
                 if (i === 0 || i === 7) {
                     board[i][j].piece = Object.create(rook);
                     board[i][j].piece.x = i;
                 } else if (i === 1 || i === 6) {
-                    board[i][j].aWhite = true;
+                    board[i][j].whiteAttack = true;
                     board[i][j].piece = Object.create(knight);
                     board[i][j].piece.x = i;
                 } else if (i === 2 || i === 5) {
-                    board[i][j].aWhite = true;
+                    board[i][j].whiteAttack = true;
                     board[i][j].piece = Object.create(bishop);
                     board[i][j].piece.x = i;
                 } else if (i === 3) {
-                    board[i][j].aWhite = true;
+                    board[i][j].whiteAttack = true;
                     board[i][j].piece = Object.create(queen);
                 } else {
-                    board[i][j].aWhite = true;
+                    board[i][j].whiteAttack = true;
                     board[i][j].piece = Object.create(king);
                 }
             }
         }
 }
 originalBoardSettings();
+let whiteKing = board[4][7].piece;
+let blackKing = board[4][0].piece;
 
 // ---------Fim tabuleiro----------
 let pieces = document.querySelectorAll('.piece');
@@ -123,20 +125,38 @@ function removeMoves(moves) {
     });
 }
 
+function getAttacks() {
+    for (let x = 0; x < 8; x++)
+        for (let y = 0; y < 8; y++)
+            board[x][y].blackAttack = board[x][y].whiteAttack = false;
+
+    for (let x = 0; x < 8; x++)
+        for (let y = 0; y < 8; y++)
+            if (board[x][y].piece != null) {
+                let attacks = board[x][y].piece.attacks();
+                attacks.forEach(attack => {
+                    board[attack[0]][attack[1]][`${board[x][y].piece.color}Attack`] = true;
+                });
+            }
+}
+
 let finalMove;
 let movesDrawn = [];
 
 pieces.forEach(pieceEl => {
     pieceEl.addEventListener('click', () => {
         removeMoves(movesDrawn);
-
+        
         let parent = pieceEl.closest('[data-square]');
         let x = parent.dataset.square.charCodeAt(0) - 97;
         let y = 8 - parent.dataset.square[1];
 
-        drawMoves(board[x][y].piece.moves());
-
-        movesDrawn = board[x][y].piece.moves();
+                
+        if (board[x][y].piece.color === turn) {
+            movesDrawn = board[x][y].piece.moves();
+            drawMoves(movesDrawn);
+        } else
+            movesDrawn = [];
     });
 
     pieceEl.addEventListener('dragstart', () => {
@@ -147,40 +167,43 @@ pieces.forEach(pieceEl => {
         let parent = pieceEl.closest('[data-square]');
         let x = parent.dataset.square.charCodeAt(0) - 97;
         let y = 8 - parent.dataset.square[1];
-        
-        drawMoves(board[x][y].piece.moves());
-
-        movesDrawn = board[x][y].piece.moves();
+        if (board[x][y].piece.color === turn) {
+            movesDrawn = board[x][y].piece.moves();
+            drawMoves(movesDrawn);
+        } else
+            movesDrawn = [];
     });
-    
+            
     pieceEl.addEventListener('dragend', () => {
         let parent = pieceEl.closest('[data-square]');
         let xPiece = parent.dataset.square.charCodeAt(0) - 97;
         let yPiece = 8 - parent.dataset.square[1];
-
+        
         let xSquare = finalMove.dataset.square.charCodeAt(0) - 97;
         let ySquare = 8 - finalMove.dataset.square[1];
         
-        for (let pieceMove of board[xPiece][yPiece].piece.moves()) {
-            if (pieceMove[0] === xSquare && pieceMove[1] === ySquare) {
-
-                board[xSquare][ySquare].piece = board[xPiece][yPiece].piece;
-                board[xSquare][ySquare].piece.x = xSquare;
-                board[xSquare][ySquare].piece.y = ySquare;
-                if (board[xSquare][ySquare].piece.moved != undefined)
+        if (board[xPiece][yPiece].piece.color === turn) {
+            for (let pieceMove of board[xPiece][yPiece].piece.moves()) {
+                if (pieceMove[0] === xSquare && pieceMove[1] === ySquare) {
+                    board[xSquare][ySquare].piece = board[xPiece][yPiece].piece;
+                    board[xSquare][ySquare].piece.x = xSquare;
+                    board[xSquare][ySquare].piece.y = ySquare;
+                    if (board[xSquare][ySquare].piece.moved != undefined)
                     board[xSquare][ySquare].piece.moved = true;
-
-                board[xPiece][yPiece].piece = null;
-                
-                removeMoves(movesDrawn);
-                movesDrawn = [];
-                
-                finalMove.innerHTML = '';
-                finalMove.appendChild(pieceEl);
-                break;
+                    
+                    board[xPiece][yPiece].piece = null;
+                    
+                    removeMoves(movesDrawn);
+                    movesDrawn = [];
+                    
+                    finalMove.innerHTML = '';
+                    finalMove.appendChild(pieceEl);
+                    getAttacks();
+                    turn = turn === 'white' ? 'black' : 'white';
+                    break;
+                }
             }
         }
-
         pieceEl.classList.remove('grabed');
     });
 });
@@ -190,9 +213,10 @@ squares.forEach(squareEl => {
         finalMove = e.currentTarget;
     });
 });
-let remove = document.getElementById('rem');
 
-remove.addEventListener('click', () => {
+let resetEl = document.getElementById('reset');
+
+resetEl.addEventListener('click', () => {
     originalBoardSettings();
     for (let i = 0; i < 64; i++) {
         let x = squares[i].dataset.square.charCodeAt(0) - 97;
--- scripts/login.js
+++ scripts/login.js
@@ -1,16 +1,141 @@
 let botaoConta = document.querySelector('#botao-nova-conta');
+let loginNomeInput = document.querySelector('#nome-logar');
+let loginSenhaInput = document.querySelector('#senha-logar');
 let userEl = document.querySelector('#usuario-novo');
 let idadeEl = document.querySelector('#idade-nova');
 let senhaEl = document.querySelector('#senha-nova');
 let botaoConfirmar = document.querySelector('#botao-confirmar');
 let botoesSumir = document.querySelectorAll('.sumir');
 let loginEl = document.querySelector('#login');
-let senhaConfirmacao = document.querySelector('#senha-confirmacao');
-let torreEl = document.querySelector('#torre-animacao');
 let senhaInput = document.querySelector('#senha-nova input');
 let senhaConfirmacaoInput = document.querySelector('#senha-confirmacao input');
 let mainEl = document.querySelector('main');
-let botaoLoginEl = document.querySelector('#login-btn');
+let loginBtnEl = document.querySelector('#botao-login');
+
+let criarContaEl = {
+    botao: document.querySelector('#botao-nova-conta'),
+    nome: document.querySelector('#novo-nome'),
+    idade: document.querySelector('#idade'),
+    senha: document.querySelector('#nova-senha'),
+    confirmacao: {
+        senha: document.querySelector('#senha-confirmacao'),
+        torre: document.querySelector('#torre-animacao'),
+    }
+}
+
+
+
+localStorage.getItem('tem usuario', false);
+
+let sumir = () => {
+    loginEl.style.transition = 'none';
+        loginEl.style.left = '-100vh';
+        userEl.style.transition = 'none';
+        userEl.style.left = '-100vh';
+        idadeEl.style.transition = 'none';
+        idadeEl.style.left = '-100vh';
+        senhaEl.style.transition = 'none';
+        senhaEl.style.left = '-100vh';
+        botaoConfirmar.style.transition = 'none';
+        botaoConfirmar.style.left = '-100vh';
+}
+
+function moveEsquerda() {
+    criarContaEl.confirmacao.torre.style.right = '60vh';
+}
+
+function apareceConfirmacao() {
+    criarContaEl.confirmacao.senha.style.transition = 'none';
+    criarContaEl.confirmacao.senha.style.top = '75vh';
+    criarContaEl.confirmacao.senha.style.right = '58vh';
+}
+
+let confirmaSenha = (validador, senha) => {
+    if(senha === validador) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+for (let botao of botoesSumir) {
+    botao.addEventListener('click', () => {
+        if (senhaInput.value != senhaConfirmacaoInput.value && senhaConfirmacaoInput.value != '') {
+            senhaConfirmacaoInput.value = '';
+            senhaEl.value = '';
+            senhaConfirmacaoInput.placeholder = 'CONFIRMAÇÃO INVÁLIDA';
+            senhaConfirmacaoInput.style.border = '0.4vh solid rgb(136, 0, 0)';
+        
+        }
+        else if(senhaConfirmacaoInput.value != ''){
+            sumir();
+            criarContaEl.confirmacao.torre.style.display = 'none';
+            senhaConfirmacaoInput.style.display = 'none';
+        }
+    });
+}
+
+let criarPerfil = () => {
+    let loginIncompleto = 0;
+    let confirmacao;
+
+    if(!criarContaEl.nome.value) {
+        criarContaEl.nome.placeholder="Digite um nome válido";
+        criarContaEl.nome.style.border="0.4vh solid rgb(136, 0, 0)";
+        
+        loginIncompleto++;
+    }
+    if(!criarContaEl.senha.value) {
+        criarContaEl.senha.placeholder="Digite uma senha válida";
+        criarContaEl.senha.style.border="0.4vh solid rgb(136, 0, 0)";
+        
+        loginIncompleto++;
+    }
+    if(!criarContaEl.idade.value) {
+        criarContaEl.idade.placeholder="Selecione uma idade válida";
+        criarContaEl.idade.style.border="0.4vh solid rgb(136, 0, 0)";
+        
+        loginIncompleto++;
+    }
+
+    if(loginIncompleto !== 0) {
+        return;
+    }
+
+    let temp;
+        criarContaEl.confirmacao.torre.style.top = '77vh';
+        temp = setTimeout(moveEsquerda, 2000);
+        temp = setTimeout(apareceConfirmacao, 4200);
+
+    botaoConfirmar.addEventListener("click", () => {
+        confirmacao = confirmaSenha(criarContaEl.senha, senhaConfirmacaoInput.value);
+    })
+
+    localStorage.removeItem('usuario');
+
+    let usuario = {
+        nome: criarContaEl.nome.value,
+        idade: idadeEl.value,
+        senha: criarContaEl.senha.value,
+        score: 0,
+        vitorias: 0,
+        derrotas: 0,
+        empates: 0,
+        tema: 'wood'
+    }
+
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+    
+    localStorage.setItem('tem usuario', JSON.stringify(true));
+
+    if(confirmacao === true) {
+        sumir(); 
+    }
+    else {
+        return;
+    }
+}
 
 botaoConta.addEventListener('click', () => {
     botaoConta.style.left = '-100vh';
@@ -24,60 +149,48 @@ botaoConta.addEventListener('click', () => {
     botaoConfirmar.style.top = '89vh';
 });
 
-function moveEsquerda() {
-    torreEl.style.right = '60vh';
+let logar = () => {
+    let loginIncompleto = 0;
+    let confirmacao;
+
+    if(!loginNomeInput.value) {
+        loginNomeInput.placeholder="Digite um nome válido";
+        loginNomeInput.style.border="0.4vh solid rgb(136, 0, 0)";
+
+        loginIncompleto++;
+    }
+    if(!loginSenhaInput.value) {
+        loginSenhaInput.placeholder="Digite uma senha válida";
+        loginSenhaInput.style.border="0.4vh solid rgb(136, 0, 0)";
+
+        loginIncompleto++;
+    }
+    if(loginIncompleto !== 0) {
+        return;
+    }
+
+    let usuario = localStorage.getItem('usuario');
+    usuario = JSON.parse(usuario);
+
+    confirmacao = confirmaSenha(usuario.senha, loginSenhaInput.value);
+
+    if(!confirmacao) {
+        loginSenhaInput.value = '';
+        loginSenhaInput.placeholder="Senha incorreta";
+        loginSenhaInput.style.border="0.4vh solid rgb(136, 0, 0)";
+
+        return;
+    }
+
+    localStorage.setItem('tem usuario', true);
+
+    selecionaTabuleiro(usuario);
+
+    sumir();
 }
 
-function apareceConfirmacao() {
-    senhaConfirmacao.style.transition = 'none';
-    senhaConfirmacao.style.top = '75vh';
-    senhaConfirmacao.style.right = '58vh';
-}
+mainEl.addEventListener('click', sumir);
 
-senhaEl.addEventListener('change', () => {
-    let temp;
-    torreEl.style.top = '77vh';
-    temp = setTimeout(moveEsquerda, 2000);
-    temp = setTimeout(apareceConfirmacao, 4200);
-});
+botaoConfirmar.addEventListener('click', criarPerfil);
 
-for (let botao of botoesSumir) {
-    botao.addEventListener('click', () => {
-        if (senhaInput.value != senhaConfirmacaoInput.value && senhaConfirmacaoInput.value != '') {
-            senhaConfirmacaoInput.value = '';
-            senhaEl.value = '';
-            senhaConfirmacaoInput.placeholder = 'CONFIRMAÇÃO INVÁLIDA';
-            senhaConfirmacaoInput.style.border = '.65vh solid red';
-
-        }
-        else if(senhaConfirmacaoInput.value != ''){
-        loginEl.style.transition = 'none';
-        loginEl.style.left = '-100vh';
-        userEl.style.transition = 'none';
-        userEl.style.left = '-100vh';
-        idadeEl.style.transition = 'none';
-        idadeEl.style.left = '-100vh';
-        senhaEl.style.transition = 'none';
-        senhaEl.style.left = '-100vh';
-        botaoConfirmar.style.transition = 'none';
-        botaoConfirmar.style.left = '-100vh';
-        torreEl.style.transition = 'none';
-        torreEl.style.left = '-100vh';
-        senhaConfirmacao.style.transition = 'none';
-        senhaConfirmacao.style.left = '-100vh';
-        }
-    });
-}
-
-mainEl.addEventListener('click', () => {
-    loginEl.style.transition = 'none';
-    loginEl.style.left = '-100vh';
-    userEl.style.transition = 'none';
-    userEl.style.left = '-100vh';
-    idadeEl.style.transition = 'none';
-    idadeEl.style.left = '-100vh';
-    senhaEl.style.transition = 'none';
-    senhaEl.style.left = '-100vh';
-    botaoConfirmar.style.transition = 'none';
-    botaoConfirmar.style.left = '-100vh';
-});
+loginBtnEl.addEventListener('click', logar);
